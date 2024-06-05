737eb5a1af4ea00b4c61e692b9e437f0ead7e454
bastndev <gohit24@gmail.com>
2024-06-04 21:01:03 -0500

    Refactor import statement for one.dart widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:update_flutter/pages/one.dart';
+import 'package:update_flutter/widgets/one.dart';
 
 void main() => runApp(const MyApp());
 
