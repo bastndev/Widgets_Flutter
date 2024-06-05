f78afaced39f82ec32d3528750fed31620be589b
bastndev <gohit24@gmail.com>
2024-06-05 17:47:53 -0500

    Refactor ListViewScreen to use dynamic list of books

--- lib/widgets/list_view.dart
+++ lib/widgets/list_view.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ListViewScreen extends StatelessWidget {
-
   const ListViewScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ListViewScreen 2'),
+    List listBook = [
+      "Book 1",
+      "Book 2",
+      "Book 3",
+      "Book 4",
+      "Book 5",
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List View Code"),
+      ),
+      body: ListView.builder(
+        itemCount: listBook.length,
+        itemBuilder: (context, index) {
+          String talk = listBook[index];
+          return ListTile(
+            leading: const Icon(Icons.arrow_back_ios),
+            title: Text(talk),
+          );
+        },
       ),
     );
   }
