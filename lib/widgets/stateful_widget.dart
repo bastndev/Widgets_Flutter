b451dde6bb45e6187d0654f9540f2e8be7feccbb
bastndev <gohit24@gmail.com>
2024-06-06 16:33:27 -0500

    chore: Refactor FulScreen widget to update UI and add dynamic name functionality

--- lib/widgets/stateful_widget.dart
+++ lib/widgets/stateful_widget.dart
@@ -10,11 +10,29 @@ class FulScreen extends StatefulWidget {
 
 // don't be afraid to use stateful widget if you need to change the state of the widget
 class _FulScreenState extends State<FulScreen> {
+  String name = "Maria";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('FulScreen 22'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(140.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(name),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    name = "John";
+                  });
+                },
+                child: const Text("Click me"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
