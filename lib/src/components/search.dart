04ff3819e658adb66c6d085446cecf1fad940cf4
bastndev <gohit24@gmail.com>
2024-06-07 01:04:12 -0500

    🔍[better] Search component to use StatefulWidget and add search functionality

--- lib/src/components/search.dart
+++ lib/src/components/search.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
   const Search({super.key});
 
   @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final TextEditingController _controller = TextEditingController();
+  bool _showClearButton = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_handleTextChanged);
+  }
+
+  void _handleTextChanged() {
+    setState(() {
+      _showClearButton = _controller.text.isNotEmpty;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_handleTextChanged);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(15.0),
         child: TextField(
+          controller: _controller,
           decoration: InputDecoration(
             filled: true,
-            fillColor: Color(0xFFebe8d6),
-            hintText: "Search ",
-            prefixIcon: Icon(Icons.search),
-            // suffixIcon: Icon(Icons.mic),
-            border: OutlineInputBorder(
+            fillColor: const Color(0xFFebe8d6),
+            prefixIcon: const Icon(Icons.search),
+            hintText: "Search",
+            suffixIcon: _showClearButton
+                ? IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      _controller.clear();
+                    },
+                  )
+                : null,
+            border: const OutlineInputBorder(
               borderRadius: BorderRadius.all(
                 Radius.circular(50.0),
               ),
