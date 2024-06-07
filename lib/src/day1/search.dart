3e450ef73a2ca5231458723a3962e0b8343e3a07
bastndev <gohit24@gmail.com>
2024-06-06 23:49:01 -0500

    [add] search and 2 icons add

--- lib/src/day1/search.dart
+++ lib/src/day1/search.dart
@@ -10,9 +10,59 @@ class SearchScreen extends StatefulWidget {
 class _FulScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('SearchScreen'),
+    return Scaffold(
+      body: Column(
+        children: [
+          const SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFFebe8d6),
+                  hintText: "Search ",
+                  prefixIcon: Icon(Icons.search),
+                  suffixIcon: Icon(Icons.mic),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50.0),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // --- --- --- --- -- Content
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 100,
+                  color: Colors.red,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  color: Colors.green,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  color: Colors.orange,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
