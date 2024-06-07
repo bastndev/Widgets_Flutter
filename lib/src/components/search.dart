f29cfc31aa8cc5ed75ae4af81a2521d4c3bbb196
bastndev <gohit24@gmail.com>
2024-06-07 00:42:55 -0500

    fireRefactor search component and add search functionality

--- lib/src/components/search.dart
+++ lib/src/components/search.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Search extends StatelessWidget {
-
   const Search({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Search'),
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xFFebe8d6),
+            hintText: "Search ",
+            prefixIcon: Icon(Icons.search),
+            suffixIcon: Icon(Icons.color_lens),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(50.0),
+              ),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:update_flutter/src/components/search.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +7,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+      body: Column(
+        children: [
+          Search(),
+        ],
       ),
     );
   }
