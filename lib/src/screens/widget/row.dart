de2b5733adc9863a76095de295120e940852d60e
bastndev <gohit24@gmail.com>
2024-06-08 14:42:53 -0500

    [add] row file

---
+++ lib/src/screens/widget/row.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Row extends StatelessWidget {
+  const Row({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Row'),
+      ),
+    );
+  }
+}
