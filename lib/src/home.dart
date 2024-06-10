f7719cee513d7ff973ab33b8e0a200d072b23aea
bastndev <gohit24@gmail.com>
2024-06-10 13:58:07 -0500

    Refactor imports and routes for better organization

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -6,43 +6,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Text(
-            'Stack Widgets 💙',
-            style: TextStyle(color: Colors.white),
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Stack Widgets 💙',
+          style: TextStyle(color: Colors.white),
         ),
-        body: ListView(
-          children: [
-            ListTile(
-              leading: const Icon(Icons.star),
-              title: const Text('Column'),
-              trailing: const Icon(Icons.arrow_forward_ios),
-              onTap: () {
-                Navigator.pushNamed(context, 'column');
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.star),
-              title: const Text('Row'),
-              trailing: const Icon(Icons.arrow_forward_ios),
-              onTap: () {
-                Navigator.pushNamed(context, 'row');
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.star),
-              title: const Text('Stack'),
-              trailing: const Icon(Icons.arrow_forward_ios),
-              onTap: () {
-                Navigator.pushNamed(context, 'stack');
-              },
-            ),
-            const Text("Hello World!"),
-            const Text("Hello World!"),
-            const Text("Hello World!"),
-          ],
-        ));
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.star),
+            title: const Text('Column'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.pushNamed(context, 'column');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.star),
+            title: const Text('Row'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.pushNamed(context, 'row');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.star),
+            title: const Text('Stack'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.pushNamed(context, 'stack');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
