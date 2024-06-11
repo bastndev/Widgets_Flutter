18c76638ffbaede99873fbf5e012a42f78395cbd
bastndev <gohit24@gmail.com>
2024-06-11 00:20:28 -0500

    feat: Add ListViewBuilder and ListViewSeparated widgets

---
+++ lib/practice/listview/listview_builder.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListViewBuilder extends StatelessWidget {
+  const ListViewBuilder({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('ListViewBuilder'),
+      ),
+    );
+  }
+}
--- lib/practice/practice.dart
+++ lib/practice/practice.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:update_flutter/practice/listview/listview_builder.dart';
+import 'package:update_flutter/practice/listview/listview_custom.dart';
+import 'package:update_flutter/practice/listview/listview_separated.dart';
 
 class Practice extends StatelessWidget {
   const Practice({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Practice'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text(
+          'Practice 🖤',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.breakfast_dining_sharp),
+            title: const Text('ListView.builder'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ListViewBuilder(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.breakfast_dining_sharp),
+            title: const Text('ListView.custom'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ListViewCustom(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.breakfast_dining_sharp),
+            title: const Text('ListView.separated'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ListViewSeparated(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
