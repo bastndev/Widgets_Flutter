5c92940fe457882d287c5b71cefa6dfae43c56ef
bastndev <gohit24@gmail.com>
2024-06-11 17:13:14 -0500

    chore: Update ListViewCustom to use ListView.custom

--- lib/practice/listview/listview_custom.dart
+++ lib/practice/listview/listview_custom.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:update_flutter/src/utils/list_box.dart';
 
 class ListViewCustom extends StatelessWidget {
   const ListViewCustom({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ListViewCustom'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Custom'),
+      ),
+      // -- -- you custom for example 10 items in list user View.custom
+      body: ListView.custom(
+        childrenDelegate: SliverChildBuilderDelegate(
+          (BuildContext context,  int index){
+            return listBox[index];
+          },
+          childCount: 10,
+        ),
       ),
     );
   }
