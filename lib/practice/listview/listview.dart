3b7324dd5aeae34d91490aaeedb2a71b882e9e82
bastndev <gohit24@gmail.com>
2024-06-11 17:51:26 -0500

    Refactor ListViewPractice to use ListView.builder

--- lib/practice/listview/listview.dart
+++ lib/practice/listview/listview.dart
@@ -2,12 +2,25 @@ import 'package:flutter/material.dart';
 
 class ListviewPractice extends StatelessWidget {
   const ListviewPractice({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ListviewPractice'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Practice'),
+      ),
+      body: ListView(
+        children: const [
+          Text("ListView Practice 0"),
+          Text("ListView Practice 1"),
+          Text("ListView Practice 2"),
+          Text("ListView Practice 3"),
+          Text("ListView Practice 4"),
+          Text("ListView Practice 5"),
+          Text("ListView Practice 6"),
+          Text("ListView Practice 7"),
+          Text("ListView Practice 8"),
+        ],
       ),
     );
   }
