d04481caf7a775e7f86d960d508376ace19a33f5
bastndev <gohit24@gmail.com>
2024-06-11 01:04:09 -0500

    chore: Refactor ListViewBuilder to use listBox from list_box.dart

--- lib/practice/listview/listview_builder.dart
+++ lib/practice/listview/listview_builder.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:update_flutter/src/utils/list_box.dart';
 
 class ListViewBuilder extends StatelessWidget {
   const ListViewBuilder({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ListViewBuilder'),
+    return Scaffold(
+      body: ListView.builder(
+        reverse: true,
+        itemCount: listBox.length,
+        padding: const EdgeInsets.only(top: 40.0),
+
+        // --- ⬇⬇⬇ that is a function that returns a widget
+        itemBuilder: (context, index) {
+          Widget element = listBox[index];
+          return element;
+        },
       ),
     );
   }
--- lib/src/utils/list_box.dart
+++ lib/src/utils/list_box.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:update_flutter/src/utils/box.dart';
 
-List<Widget> widgets = [
+// list of widgets
+List<Widget> listBox = [
   const Box(
     color: Colors.red,
     number: 1,
@@ -24,42 +25,42 @@ List<Widget> widgets = [
   ),
   const Box(
     color: Colors.red,
-    number: 1,
+    number: 5,
     broad: 100.0,
   ),
   const Box(
     color: Colors.blue,
-    number: 2,
+    number: 6,
     broad: 100.0,
   ),
   const Box(
     color: Colors.yellow,
-    number: 3,
+    number: 7,
     broad: 100.0,
   ),
   const Box(
     color: Colors.orange,
-    number: 4,
+    number: 8,
     broad: 100.0,
   ),
   const Box(
     color: Colors.red,
-    number: 1,
-    broad: 100.0,
-  ),
-  const Box(
-    color: Colors.blue,
-    number: 2,
-    broad: 100.0,
-  ),
-  const Box(
-    color: Colors.yellow,
-    number: 3,
-    broad: 100.0,
-  ),
-  const Box(
-    color: Colors.orange,
     number: 9,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.blue,
+    number: 10,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.yellow,
+    number: 11,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.orange,
+    number: 12,
     broad: 100.0,
   ),
 ];
