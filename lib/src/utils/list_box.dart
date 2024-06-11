6812ec8e007dcb1bc2540657c3dce8f33190222b
bastndev <gohit24@gmail.com>
2024-06-10 23:38:24 -0500

    [add] list_box to my project

---
+++ lib/src/utils/list_box.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:update_flutter/src/utils/box.dart';
+
+List<Widget> widgets = [
+  const Box(
+    color: Colors.red,
+    number: 1,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.blue,
+    number: 2,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.yellow,
+    number: 3,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.orange,
+    number: 4,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.red,
+    number: 1,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.blue,
+    number: 2,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.yellow,
+    number: 3,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.orange,
+    number: 4,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.red,
+    number: 1,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.blue,
+    number: 2,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.yellow,
+    number: 3,
+    broad: 100.0,
+  ),
+  const Box(
+    color: Colors.orange,
+    number: 9,
+    broad: 100.0,
+  ),
+];
