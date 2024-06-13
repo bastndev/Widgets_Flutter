fdb004cdebada976279e3838c0064031c2740015
bastndev <gohit24@gmail.com>
2024-06-13 18:17:05 -0500

    chore: Refactor list.dart to use List.filled for creating a list with 5 elements

--- lib/practice/Lists/list.dart
+++ lib/practice/Lists/list.dart
@@ -6,9 +6,15 @@ void main() {
 }
  */
 
-// --- --- List with List.generate , multiplica el 2 [0,2,4,6,8]
-var list = List.generate(5, (index) => index  * 2);
+// --- --- List with List.generate |  multiplica el 2 [0,2,4,6,8]
+/* var list = List.generate(5, (index) => index  * 2);
 
 void main() {
+  print("List: $list");
+} */
+
+// --- --- -- List.filled  |  [0,0,0,0,0] create a list with 5 elements with value 0
+void main() {
+  var list = List.filled(5, 0);
   print("List: $list");
 }
