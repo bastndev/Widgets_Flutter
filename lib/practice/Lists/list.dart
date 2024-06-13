a8fa50fb7c0fa26d66bd5243833ae4b962de14ef
bastndev <gohit24@gmail.com>
2024-06-13 18:30:01 -0500

    Refactor code to use List.reduce for summing elements in intList

--- lib/practice/Lists/list.dart
+++ lib/practice/Lists/list.dart
@@ -13,8 +13,28 @@ void main() {
   print("List: $list");
 } */
 
-// --- --- -- List.filled  |  [0,0,0,0,0] create a list with 5 elements with value 0
+/* // --- --- -- List.filled  |  [0,0,0,0,0] create a list with 5 elements with value 0
 void main() {
   var list = List.filled(5, 0);
   print("List: $list");
+} */
+
+/* // --- --- -- List.unmodifiable |  Error: No se puede modificar una lista inmutable
+void main() {
+  var list = List.unmodifiable([1, 2, 3, 4, 5]);
+// list.add(6); 
+} */
+
+/* // --- --- -- List.from y List.of: Crea listas a partir de otros iterables.
+void main() {
+var anotherList = [1, 2, 3];
+var list = List.from(anotherList); // [1, 2, 3]
+} */
+
+// --- --- -- List reduce: Reducir los elementos de una lista a un solo valor mediante una función de acumulación.
+void main() {
+  List<int> intList = [1, 2, 3, 4, 5];
+  int sum = intList.reduce((value, element) => value + element);
+
+  print(sum); // 15
 }
