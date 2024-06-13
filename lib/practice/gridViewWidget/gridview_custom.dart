6caa031322ca1f5d6e0010822f312355811904df
bastndev <gohit24@gmail.com>
2024-06-12 23:31:55 -0500

    chore: Refactor GridViewWidget to use GridView.builder and update gridview_count.dart

--- lib/practice/gridViewWidget/gridview_custom.dart
+++ lib/practice/gridViewWidget/gridview_custom.dart
@@ -2,12 +2,32 @@ import 'package:flutter/material.dart';
 
 class GridViewCustom extends StatelessWidget {
   const GridViewCustom({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('GridViewCustom'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GridView.custom'),
+      ),
+      body: GridView.custom(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 1/2,
+          // childAspectRatio: 3 / 2,
+        ),
+        childrenDelegate: SliverChildBuilderDelegate(
+          (BuildContext context, int index) {
+            return Container(
+              color: Colors.purple[100 * (index % 9)],
+              child: Center(
+                child: Text('Item $index'),
+              ),
+            );
+          },
+          childCount: 30,
+        ),
       ),
     );
   }
