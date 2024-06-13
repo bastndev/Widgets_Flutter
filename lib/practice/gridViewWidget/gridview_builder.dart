45dd620755c2339298dcc75b15f828c9dc200070
bastndev <gohit24@gmail.com>
2024-06-12 23:27:33 -0500

    chore: Refactor GridViewWidget to use GridView.builder and update gridview_count.dart

--- lib/practice/gridViewWidget/gridview_builder.dart
+++ lib/practice/gridViewWidget/gridview_builder.dart
@@ -13,13 +13,16 @@ class GridViewBuilder extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          // childAspectRatio: 1/4,
+          childAspectRatio: 3/2,
+
         ),
-        itemCount: 10,
+        itemCount: 30,
         itemBuilder: (BuildContext context, int index) {
           return Container(
-            color: Colors.blue[100 * (index % 9)],
+            color: Colors.orange[100 * (index % 9)],
             child: Center(
               child: Text('Item $index'),
             ),
--- lib/practice/gridViewWidget/gridview_count.dart
+++ lib/practice/gridViewWidget/gridview_count.dart
@@ -2,12 +2,28 @@ import 'package:flutter/material.dart';
 
 class GridViewCount extends StatelessWidget {
   const GridViewCount({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('GridViewCount'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GridView.count'),
+      ),
+      body: GridView.count(
+        padding: const EdgeInsets.all(10.0),
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        // childAspectRatio: 1/4,
+        childAspectRatio: 3/2,
+        children: List.generate(9, (index) {
+          return Container(
+            color: Colors.orange[100 * (index % 9)],
+            child: Center(
+              child: Text('Item $index'),
+            ),
+          );
+        }),
       ),
     );
   }
