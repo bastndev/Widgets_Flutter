7fa51ef8e1b5cdc86c815aeda78c661e98c5d762
bastndev <gohit24@gmail.com>
2024-06-13 20:57:04 -0500

    [add] widget image of no internet

--- lib/src/widgets/images.dart
+++ lib/src/widgets/images.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
   const ImageWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ImageWidget'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ImageWidget'),
       ),
+      body: Image.asset('assets/images/no_internet.png'),
     );
   }
 }
