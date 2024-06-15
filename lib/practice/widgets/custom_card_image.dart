694fda2a4b3e7ea778359705e1158bbcf2196a28
bastndev <gohit24@gmail.com>
2024-06-14 22:54:02 -0500

    feat: Update CustomCardImage widget to display multiple images with lazy loading

--- lib/practice/widgets/custom_card_image.dart
+++ lib/practice/widgets/custom_card_image.dart
@@ -14,9 +14,38 @@ class CustomCardImage extends StatelessWidget {
                 NetworkImage('https://avatars.githubusercontent.com/bastndev'),
             placeholder: AssetImage('assets/images/loading.webp'),
           ),
-          Image(
-              image: NetworkImage(
-                  "https://avatars.githubusercontent.com/bastndev")),
+          SizedBox(
+            height: 10,
+          ),
+          FadeInImage(
+            image: NetworkImage(
+                'https://mb.cision.com/Public/17861/2698634/adbd256798f87eb9_400x400ar.jpg'),
+            placeholder: AssetImage('assets/images/loading.webp'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FadeInImage(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/25662349/pexels-photo-25662349/free-photo-of-mercado-nocturno-de-temple-street-hong-kong.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+            placeholder: AssetImage('assets/images/loading.webp'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FadeInImage(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/20687864/pexels-photo-20687864/free-photo-of-noche-rascacielos-viaje-viajar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+            placeholder: AssetImage('assets/images/loading.webp'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FadeInImage(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/15700949/pexels-photo-15700949/free-photo-of-ciudad-rascacielos-viaje-viajar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+            placeholder: AssetImage('assets/images/loading.webp'),
+          ),
         ],
       ),
     );
