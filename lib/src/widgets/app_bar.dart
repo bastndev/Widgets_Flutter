ffc01ea5f3886eb6e340365e75a27739e3392944
bastndev <gohit24@gmail.com>
2024-06-17 22:15:58 -0500

    Update analysis_options.yaml to ignore deprecated member use errors

--- lib/src/widgets/app_bar.dart
+++ lib/src/widgets/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class AppBarWidget extends StatelessWidget {
   const AppBarWidget({super.key});
@@ -10,12 +11,23 @@ class AppBarWidget extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.yellow,
-        title: const Row(
+        title:  Row(
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: [
-            FlutterLogo(),
-            Padding(
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              },
+              child: SvgPicture.asset(
+                'assets/images/icons/arrow-left.svg',
+                height: 20,
+                width: 20,
+                color: Colors.black,
+              ),
+            ),
+            // FlutterLogo(),
+            const Padding(
               padding: EdgeInsets.only(left: 16.0),
               child: Text(
                 "Title with image",
