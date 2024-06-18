17942ec2ff470d4f3861a6e9fc5884c8e8956f23
bastndev <gohit24@gmail.com>
2024-06-17 23:43:26 -0500

    Update DrawerWidget with custom body content

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawer Widget'),
+      ),
+      drawer: Drawer(
+        child: ListView(),
+      ),
+
+      // --- --- --- --- --- Content here || body
       body: Center(
-        child: Text('DrawerWidget 33'),
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.purple.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: const Center(
+            child: Text('Body and content here !'),
+          ),
+        ),
       ),
     );
   }
