15a6518276c2ab8a76e2d1c314b3a29284ceabe9
bastndev <gohit24@gmail.com>
2024-06-19 00:56:40 -0500

    Update DrawerWidget with custom leading icon and functionality

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key});
@@ -8,13 +9,36 @@ class DrawerWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Drawer Widget'),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return InkWell( //-TODO: Student later
+              onTap: () {
+                Scaffold.of(context).openDrawer();
+              },
+              child: SizedBox(
+                // These values no longer affect the SVG size directly
+                height: 1.0,
+                width: 121.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SvgPicture.asset(
+                    'assets/images/icons/energy.svg',
+                    width: 19.0, // Set your desired width
+                    height: 2.0, // Set your desired height
+                    // color: Colors.blue,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
       drawer: Drawer(
         child: ListView(
           children: [
             Align(
               child: Padding(
-                padding:  const EdgeInsets.all(1.0),
+                padding: const EdgeInsets.all(1.0),
                 child: Container(
                   decoration: BoxDecoration(
                     color: Colors.red,
@@ -25,7 +49,8 @@ class DrawerWidget extends StatelessWidget {
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(10.0),
                     child: const Image(
-                      image: NetworkImage('https://avatars.githubusercontent.com/u/113950039?v=4'),
+                      image: NetworkImage(
+                          'https://avatars.githubusercontent.com/u/113950039?v=4'),
                       fit: BoxFit.cover,
                     ),
                   ),
