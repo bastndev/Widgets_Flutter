151c6ab59c50f4a25b52959b2e91019165c3a84f
bastndev <gohit24@gmail.com>
2024-06-20 14:19:08 -0500

    refactor: Improve drawer layout and image profile in DrawerPractice widget

--- lib/practice/widgets/drawer2_practice.dart
+++ lib/practice/widgets/drawer2_practice.dart
@@ -16,54 +16,53 @@ class DrawerPractice extends StatelessWidget {
             color: Colors.yellow[200],
             child: ListView(
               children: [
-                Container(
-                  color: Colors.blue[200], //-TODO: practice better x and picture , add email? and bitel design
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0, top: 10.0),
-                    child: Align(
-                      alignment: Alignment.topRight, // Align the "X" icon to the top right
-                      child: IconButton(
-                        icon: const Icon(Icons.close),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                    ),
-                  ),
-                ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 20.0, top: 10.0),
+                  padding: const EdgeInsets.only(left: 15.0, top: 10.0),
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.black12,
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          width: 70.0,
-                          height: 70.0,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: const FadeInImage(
-                              image: NetworkImage(
-                                  'https://avatars.githubusercontent.com/u/113950039?v=4'),
-                              fit: BoxFit.cover,
-                              placeholder: AssetImage('assets/images/me.jpg'),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 40.0),
-                          child: Align(
-                            alignment: Alignment.bottomLeft,
-                            child: Text(
-                              'Gohit Bastian',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 25,
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              width: 70.0,
+                              height: 70.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: const FadeInImage(
+                                  image: NetworkImage(
+                                      'https://avatars.githubusercontent.com/u/113950039?v=4'),
+                                  fit: BoxFit.cover,
+                                  placeholder: AssetImage('assets/images/me.jpg'),
+                                ),
                               ),
                             ),
+                            const SizedBox(width: 10),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 40.0),
+                              child: Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Text(
+                                  'Gohit Bastian',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 25,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 40.0),
+                          child: IconButton(
+                            icon: const Icon(Icons.close),
+                            onPressed: () => Navigator.pop(context),
                           ),
                         ),
                       ],
