0b5de56102d3b723a338c1ab352fc5dc7476ad49
bastndev <gohit24@gmail.com>
2024-06-19 22:47:56 -0500

    [better] drawer

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -31,22 +31,40 @@ class DrawerWidget extends StatelessWidget {
                 padding: const EdgeInsets.only(left: 20.0, top: 10.0),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    width: 70.0,
-                    height: 70.0,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: const FadeInImage(
-                        image: NetworkImage(
-                            'https://avatars.githubusercontent.com/u/113950039?v=4'),
-                        fit: BoxFit.cover,
-                        placeholder: AssetImage('assets/images/me.jpg'),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black12,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        width: 70.0,
+                        height: 70.0,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: const FadeInImage(
+                            image: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/113950039?v=4'),
+                            fit: BoxFit.cover,
+                            placeholder: AssetImage('assets/images/me.jpg'),
+                          ),
+                        ),
                       ),
-                    ),
+                      const SizedBox(width: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40.0),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Text(
+                            'Gohit Bastian',
+                            style: TextStyle(
+                              color: Colors.grey[200],
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
