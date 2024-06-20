4580acdf16e2746cf2082c3d88780805bf4d53ea
bastndev <gohit24@gmail.com>
2024-06-19 21:53:54 -0500

    [add] padding to my image profile

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -23,38 +23,42 @@ class DrawerWidget extends StatelessWidget {
         ),
       ),
       drawer: Drawer(
-        child: ListView(
-          children: [
-            Align(
-              child: Padding(
-                padding: const EdgeInsets.all(1.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  width: 70.0,
-                  height: 70.0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: const FadeInImage(
-                      image: NetworkImage(
-                          'https://avatars.githubusercontent.com/u/113950039?v=4'),
-                      fit: BoxFit.cover,
-                      placeholder: AssetImage('assets/images/me.jpg'),
+        child: Container(
+          color: Colors.purple[200],
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    width: 70.0,
+                    height: 70.0,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: const FadeInImage(
+                        image: NetworkImage(
+                            'https://avatars.githubusercontent.com/u/113950039?v=4'),
+                        fit: BoxFit.cover,
+                        placeholder: AssetImage('assets/images/me.jpg'),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            ListTile(
-              title: const Text('Home'),
-              leading: const Icon(Icons.home),
-              onTap: () {
-                Navigator.pushNamed(context, '/');
-              },
-            ),
-          ],
+              ListTile(
+                title: const Text('Home'),
+                leading: const Icon(Icons.home),
+                onTap: () {
+                  Navigator.pushNamed(context, '/');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
