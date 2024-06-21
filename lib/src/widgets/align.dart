2f693762e91bd5d7f96c5a3746422c119dac1804
bastndev <gohit24@gmail.com>
2024-06-20 23:12:33 -0500

    refactor: Update AlignWidget to use AppBar and BlueBox for better layout

--- lib/src/widgets/align.dart
+++ lib/src/widgets/align.dart
@@ -6,6 +6,77 @@ class AlignWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          decoration: containerWidget(),
+          child: const Stack(
+            children: [
+              Align(
+                child: CustomBox(color: Colors.red),
+              ),
+              Align(
+                alignment: Alignment(0, -0.5),
+                child: CustomBox(color: Colors.green),
+              ),
+              Align(
+                alignment: Alignment(0, 0.5),
+                child: CustomBox(color: Colors.green),
+              ),
+              Align(
+                alignment: Alignment(-1, 1),
+                child: CustomBox(color: Colors.blue),
+              ),  
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration containerWidget() {
+    return BoxDecoration(
+      border: Border.all(
+        color: Colors.blueAccent,
+        width: 2,
+      ),
+    );
+  }
+}
+
+class CustomBox extends StatelessWidget {
+  final Color color;
+
+  const CustomBox({
+    super.key,
+    this.color = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      width: 50,
+      height: 50,
+    );
+  }
+}
+
+
+
+
+
+
+
+/* import 'package:flutter/material.dart';
+
+class AlignWidget extends StatelessWidget {
+  const AlignWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text('AlignWidget'),
       ),
@@ -29,3 +100,4 @@ class BlueBox extends StatelessWidget {
     );
   }
 }
+ */
