194598576cd6f6a742c19e2f8445d6b933d64e42
bastndev <gohit24@gmail.com>
2024-06-20 22:45:23 -0500

    refactor: Update AlignWidget to use AppBar and BlueBox for better layout

--- lib/src/widgets/align.dart
+++ lib/src/widgets/align.dart
@@ -5,10 +5,27 @@ class AlignWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AlignWidget'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AlignWidget'),
       ),
+      body: const Align(
+        alignment: Alignment(-1,1),
+        child: BlueBox(),
+      ),
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  const BlueBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      width: 50,
+      height: 50,
     );
   }
 }
