e7330bb6e9e552b8be2f98b6aaf27af5aca76f5e
bastndev <gohit24@gmail.com>
2024-07-12 15:57:21 -0500

    refactor: Update ButtonNavigationBar to use BottomNavigationBar for navigation

--- lib/src/widgets/button_navigation_bar.dart
+++ lib/src/widgets/button_navigation_bar.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
-class ButtonNavigationBar extends StatelessWidget {
+class ButtonNavigationBar extends StatefulWidget {
   const ButtonNavigationBar({super.key});
-  
+
+  @override
+  State<ButtonNavigationBar> createState() => _ButtonNavigationBarState();
+}
+
+class _ButtonNavigationBarState extends State<ButtonNavigationBar> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('ButtonNavigationBar 1'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // type: BottomNavigationBarType.shifting, //change background
+        currentIndex: selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              activeIcon: Icon(Icons.more),
+              label: 'Search',
+              backgroundColor: Colors.red),
+        ],
       ),
     );
   }
