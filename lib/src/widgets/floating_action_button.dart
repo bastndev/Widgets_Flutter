019f15854a79f199b7f99bf5c3c0fde11f16ab95
bastndev <gohit24@gmail.com>
2024-07-13 14:32:34 -0500

    refactor: Update Floating Action Button widget to use StatefulWidget

--- lib/src/widgets/floating_action_button.dart
+++ lib/src/widgets/floating_action_button.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class FloatingActionBtn extends StatelessWidget {
+class FloatingActionBtn extends StatefulWidget {
   const FloatingActionBtn({super.key});
-  
+
+  @override
+  State<FloatingActionBtn> createState() => _FloatingActionBtnState();
+}
+
+class _FloatingActionBtnState extends State<FloatingActionBtn> {
+  int number = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Floating Action Button'),
+      ),
       body: Center(
-        child: Text('FloatingActionBtn 22'),
+        child: Text('Hello Mayer! $number'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            number++;
+          });
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
       ),
     );
   }
