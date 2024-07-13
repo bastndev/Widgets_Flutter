54e23307025cb3e12cfc6ed74a883ca44739a92a
bastndev <gohit24@gmail.com>
2024-07-13 18:27:34 -0500

    feat: Add TextFieldWidget with various input fields

--- lib/src/widgets/text_field.dart
+++ lib/src/widgets/text_field.dart
@@ -1,13 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 
 class TextFieldWidget extends StatelessWidget {
   const TextFieldWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TextFieldWidget'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          'TextField Widget',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body:  Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFFebe8d6),
+                  prefixIcon: Icon(Icons.search),
+                  hintText: "Search",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50.0),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  suffixIcon: Icon(Icons.date_range),
+                  labelText: 'Date',
+                  hintText: 'mm/dd/yyyy',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  hintText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  hintText: 'Enter your email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.phone),
+                  hintText: 'Enter your phone number',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                maxLines: 5,
+                decoration: InputDecoration(
+                  hintText: 'Enter your message',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter a number',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+                keyboardType: TextInputType.number, //keyboard Number
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly]// enter here only number no world,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
