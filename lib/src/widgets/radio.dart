e3fe5ed999de5e414e5b335eb91e83a582d47527
bastndev <gohit24@gmail.com>
2024-07-15 17:12:37 -0500

    🥥[better] Radio select Yes or No

--- lib/src/widgets/radio.dart
+++ lib/src/widgets/radio.dart
@@ -8,16 +8,58 @@ class RadioWidget extends StatefulWidget {
 }
 
 class _RadioWidgetState extends State<RadioWidget> {
+  String groupValue = "Yes";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black26,
-        title: const Text('Radio Button in Flutter '),
+        title: const Text('Radio Button in Flutter'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('RadioWidget '),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            const Center(
+              child: Text(
+                "Are you developer ?",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Radio<String>(
+                  value: "Yes",
+                  groupValue: groupValue,
+                  onChanged: (value) {
+                    setState(() {
+                      groupValue = value!;
+                    });
+                  },
+                ),
+                const Text("Yes"),
+                Radio<String>(
+                  value: "No",
+                  groupValue: groupValue,
+                  onChanged: (value) {
+                    setState(() {
+                      groupValue = value!;
+                    });
+                  },
+                ),
+                const Text("No")
+              ],
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Submit"),
+            )
+          ],
+        ),
       ),
     );
   }
