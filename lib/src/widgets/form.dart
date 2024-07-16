65be2c1389dd9bb8c028374d09bd4725de6449bd
bastndev <gohit24@gmail.com>
2024-07-16 17:03:31 -0500

    feat: Add form validation to FormWidget

--- lib/src/widgets/form.dart
+++ lib/src/widgets/form.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
 
+final _formkey = GlobalKey<FormState>();
+
 class FormWidget extends StatelessWidget {
   const FormWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('FormWidget'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          'FormWidget',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter something';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter something';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('check ✅'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
