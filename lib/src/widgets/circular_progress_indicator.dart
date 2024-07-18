b984b52fd3570f7e7f425f980292da8e993e4e1c
bastndev <gohit24@gmail.com>
2024-07-17 20:16:51 -0500

    chore: Update CircularProgressWidget with new styling and app bar

--- lib/src/widgets/circular_progress_indicator.dart
+++ lib/src/widgets/circular_progress_indicator.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 class CircularProgressWidget extends StatelessWidget {
   const CircularProgressWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CircularProgressWidget'),
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('CircularProgressWidget'),
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(
+          color: Colors.blueGrey,
+          backgroundColor: Colors.orange,
+        ),
       ),
     );
   }
