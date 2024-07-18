2b5cd5e78b64e2a5a0a32b29db274ddee6ab4c07
bastndev <gohit24@gmail.com>
2024-07-18 14:50:13 -0500

    Refactor LinearProgressWidget to use StatefulWidget and AnimationController

--- lib/src/widgets/linear_progress_indicator.dart
+++ lib/src/widgets/linear_progress_indicator.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
-class LinearProgressWidget extends StatelessWidget {
+class LinearProgressWidget extends StatefulWidget {
   const LinearProgressWidget({super.key});
+
+  @override
+  State<LinearProgressWidget> createState() => _LinearProgressWidgetState();
+}
+
+class _LinearProgressWidgetState extends State<LinearProgressWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    _controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +35,26 @@ class LinearProgressWidget extends StatelessWidget {
       appBar: AppBar(
         title: const Text('LinearProgressWidget'),
       ),
-      body: const Center(
-        child: Text('LinearProgressWidget'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              LinearProgressIndicator(
+                color: Colors.red,
+                value: _controller.value,
+                backgroundColor: Colors.black,
+              ),
+              Text('${(_controller.value * 100).toStringAsFixed(0)}%'),
+              const SizedBox(height: 20),
+              LinearProgressIndicator(
+                value: _controller.value,
+              ),
+              Text('${(_controller.value * 100).toStringAsFixed(0)}%'),
+            ],
+          ),
+        ),
       ),
     );
   }
