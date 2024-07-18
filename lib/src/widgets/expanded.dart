fc9f32739b62c8a7ba86b48f22a76df72169ff29
bastndev <gohit24@gmail.com>
2024-07-18 15:59:05 -0500

    feat: Update ExpandedWidget to use Row with expanded children

--- lib/src/widgets/expanded.dart
+++ lib/src/widgets/expanded.dart
@@ -6,26 +6,37 @@ class ExpandedWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text(
-          'ExpandedWidget',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'ExpandedWidget',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-      body: const Square(Colors.blue),
-    );
+        body: const Row(
+          children: [
+            Square(Colors.blue),
+            Expanded(
+              flex: 3,
+              child: Square(Colors.red),
+            ),
+            Expanded(
+              child: Square(Colors.red),
+            ),
+            Square(Colors.blue),
+          ],
+        ));
   }
 }
 
 class Square extends StatelessWidget {
   final Color color;
-
   const Square(this.color, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       width: 50,
       height: 50,
       decoration: BoxDecoration(
