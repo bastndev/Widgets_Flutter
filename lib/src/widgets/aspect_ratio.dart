ae7f04d8a7502b515e1012ccae9b21f701d0693b
bastndev <gohit24@gmail.com>
2024-07-22 17:02:03 -0500

    feat: Update AspectRatioWidget to include an app bar and adjust styling

--- lib/src/widgets/aspect_ratio.dart
+++ lib/src/widgets/aspect_ratio.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 
 class AspectRatioWidget extends StatelessWidget {
   const AspectRatioWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AspectRatioWidget 111111'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AspectRatioWidget'),
+      ),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Container(
+      color: Colors.orangeAccent,
+      alignment: Alignment.center,
+      width: double.infinity,
+      // height: 150,
+      height: 300,
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Container(
+          color: Colors.blueGrey,
+        ),
       ),
     );
   }
