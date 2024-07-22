5c0db74ca038c7deb9aa908e193ab5e7e12ac230
bastndev <gohit24@gmail.com>
2024-07-22 16:41:05 -0500

    feat: Add CustomPageWidget and DemoPainter to display a custom page with a Pacman shape

--- lib/src/widgets/custom_page.dart
+++ lib/src/widgets/custom_page.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CustomPageWidget extends StatelessWidget {
   const CustomPageWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CustomPageWidget 333'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Custom Page Widget'),
+      ),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: CustomPaint(
+          painter: DemoPainter(),
+          child: const Text(
+            'This is a Packman',
+            style: TextStyle(
+              color: Colors.black,
+              backgroundColor: Colors.white54,
+              fontSize: 30.0,
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
+
+class DemoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var center = size / 2;
+    var paint = Paint()..color = Colors.yellow;
+    canvas.drawArc(
+      Rect.fromCenter(
+        center: Offset(center.width, center.height),
+        width: 250,
+        height: 250,
+      ),
+      0.4,
+      2 * 3.14 - 0.8,
+      true,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
